2ca2375dcb7e11901871e423d6e97f33eb90736d
prututu <castd650@newschool.edu>
2023-02-28 16:01:42 -0500

    A lot of stuff

--- assets/main.js
+++ assets/main.js
@@ -2,3 +2,8 @@ window.arenaCallback = () => {
 	// Put your JavaScript in here!
 	// This will run after Are.na’s API returns your data.
 }
+
+// Get all of my images blocks
+let imageBlocks = document.querySelectorAll('li.image-block')
+console
+// 
